19455d2b5683b055cf744d7c7ccfe7b1ba85d8f3
wallisonstorck <storck779@gmail.com>
2021-06-25 22:54:04 -0400

    part lesson 4

---
+++ public/scripts/main.js
@@ -0,0 +1,30 @@
+import Modal from './modal.js'; //Importa a função
+
+const modal = Modal(); //Guarda a função na constante.
+
+//Mapeando para usar modal para confirmar leitura também
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
+const checkButton = document.querySelectorAll('.actions a.check');
+checkButton.forEach(element => {
+   element.addEventListener('click', handleClick);
+});
+
+//Quando o botão delete for clicado abre a modal para então confirmar 
+const deleteButton = document.querySelectorAll('.actions a.delete');
+deleteButton.forEach(element => {
+   element.addEventListener('click', (event) => handleClick(event, false));
+});
+
+function handleClick(event, check = true) {
+   event.preventDefault(); //Bloqueia o formulário para nao mudar de página
+   const text = check ? "Deseja marcar como lida" : "Tem certeza que você deseja excluir";
+   modalTitle.innerHTML = check ? "Marcar pergunta como lida" : "Excluir pergunta";
+   modalDescription.innerHTML = check ? `${text} esta pergunta?` : `${text} esta pergunta?`;
+   modalButton.textContent = check ? "Sim, marcar" : "Sim, excluir";
+   check ? modalButton.classList.remove('red') : modalButton.classList.add('red');
+   modal.open();
+}
+
---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express'); //Importa o express para a constante.
+const route = require('./route');
+const path = require('path'); // (Módulo)
+
+const server = express(); //Guarda o express já iniciado.
+
+server.set('view engine', 'ejs');
+
+server.use(express.static('public'));
+
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(route);
+
+server.listen(3000, () => console.log('Server is started!'));
